7813da812ecf4e9d6983ceeb1295fa73783ecb66
정하경 <hk100456@naver.com>
2025-12-04 16:43:25 +0900

    오류

--- FrontGUI/MyFrame.java
+++ FrontGUI/MyFrame.java
@@ -13,28 +13,5 @@ public class MyFrame extends JFrame
 {
     LibraryApplication la;
     private JTextArea logTA;
-    
-    public MyFrame(){
-        this.setTitle("과제#2");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000,500);
-        this.setLayout(new GridLayout(1,3));
-        
-        la = new LibraryApplication();
-        
-        logTA = new JTextArea("[출력화면]\n");
-        logTA.setEditable(false);
-        
-        JScrollPane logSP = new JScrollPane(logTA);
-        
-        WestPanel westP = new WestPanel(logTA, la);
-        CenterPanel centerP = new CenterPanel(logTA, la);
-        //EastPanel eastP = new EastPanel();
-        
-        this.add(westP);
-        this.add(centerP);
-        this.add(logSP);
-        
-        this.setVisible(true);
-    }
+
 }
