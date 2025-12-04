905d6cdcbc3f91da630ba6ad349472f9f0dd2514
정하경 <hk100456@naver.com>
2025-12-04 16:44:23 +0900

    FrontGUI와 CoreEngine 연결

--- FrontGUI/MyFrame.java
+++ FrontGUI/MyFrame.java
@@ -17,16 +17,18 @@ public class MyFrame extends JFrame
     public MyFrame(){
         this.setTitle("과제#2");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(900,500);
+        this.setSize(1000,500);
         this.setLayout(new GridLayout(1,3));
         
-        logTA = new JTextArea("[출력 화면]\n");
+        la = new LibraryApplication();
+        
+        logTA = new JTextArea("[출력화면]\n");
         logTA.setEditable(false);
         
         JScrollPane logSP = new JScrollPane(logTA);
         
-        WestPanel westP = new WestPanel(logTA);
-        CenterPanel centerP = new CenterPanel(logTA);
+        WestPanel westP = new WestPanel(logTA, la);
+        CenterPanel centerP = new CenterPanel(logTA, la);
         //EastPanel eastP = new EastPanel();
         
         this.add(westP);
